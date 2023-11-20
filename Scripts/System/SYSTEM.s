d9f4ecceb8cb09afdc04d980841dfa1ca8509209
vimegar <151482187+vimegar@users.noreply.github.com>
2023-11-19 23:50:30 -0800

    Fix turn order bug
    The original code has a bug calculating turn order where instead of
    giving -10 speed, the luck chance modifier forces the creature to go
    last.
    Credit to Gau Cho for identifying this bug while decompiling the game
    before the source was available!

--- Scripts/System/SYSTEM.s
+++ Scripts/System/SYSTEM.s
@@ -1,4 +1,4 @@
-;Size:427
+;Size:431
                       
 ;********************************
 ; C:\Users\vimegar\Documents\Dev\MagiNationSource\SCRIPTS\SYSTEM\SYSTEM.s
@@ -373,6 +373,10 @@ _ARD
 ;********************************
 ?_MAIN
 	DB	CMD_JUMP
+		DB	:?_WARPPORTAL
+		DW	(?_WARPPORTAL&$FFFF)
+
+	DB	CMD_JUMP
 		DB	:?_MAIN_TITLES
 		DW	(?_MAIN_TITLES&$FFFF)
 
@@ -428,6 +432,7 @@ _ARD
 	GLOBAL	?_OVRSURFACE03_LOAD
 	GLOBAL	?_OVRSURFACE04_LOAD
 	GLOBAL	?_OVRSURFACE05_LOAD
+	GLOBAL	?_WARPPORTAL
 	GLOBAL	?_MAIN_TITLES
 	GLOBAL	PAL_HERO
 	GLOBAL	?HERO_AI
